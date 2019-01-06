2cb1f1a88536c9a1b6b0e0ab8a4914173decf092
wstars <545640807@qq.com>
2019-01-06 22:00:10 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="container">
+		<el-row :gutter="20">
+			<el-col :span="12" :offset="6">
+				<el-input v-model="input" class="search-input" placeholder="输入查询内容" :suffix-icon="searchIcon" clearable></el-input>
+			</el-col>
+		</el-row>
+		<el-row>
+			<el-col :span="3" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 2 : 0">
+				<el-card shadow="never" :body-style="{ padding: '0px' }">
+					<img src="../assets/windows.jpg" class="image">
+					<div style="padding: 8px;">
+						<span>Windows</span>
+					</div>
+				</el-card>
+			</el-col>
+		</el-row>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: 'index',
+		data() {
+			return {
+				input: '',
+				searchIcon: 'el-icon-search'
+			}
+		},
+		methods: {}
+	}
 </script>
+<style>
+.container {
+	padding-top: 10px;
+}
+
+.container .search-input .el-input__inner {
+	height: 45px !important
+}
+
+
+.image {
+	width: 100%;
+	display: block;
+}
+
+.clearfix:before,
+.clearfix:after {
+	display: table;
+	content: "";
+}
+
+.clearfix:after {
+	clear: both
+}
+</style>
